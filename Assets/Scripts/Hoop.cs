c43a5ae68eff36c3e535283a033177e1bbff8dc5
Koen Sparreboom <koensparreboom@gmail.com>
2019-02-07 22:04:30 +0100

    Initial commit

---
+++ Assets/Scripts/Hoop.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Hoop : MonoBehaviour
+{
+    [SerializeField] private Text scoreText;
+    private int _score;
+    
+    [SerializeField] private AudioClip scoreClip;
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Rigidbody>().useGravity)
+        {
+            _score++;
+            scoreText.text = "Score: " + _score;
+            
+            _audioSource.PlayOneShot(scoreClip);
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/IgnorePlayerCollider.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class IgnorePlayerCollider : MonoBehaviour
+{
+    private void Start()
+    {
+        Physics.IgnoreCollision(GetComponent<Collider>(), GameObject.FindWithTag("Player").GetComponent<Collider>());
+    }
+}
